0027ee108eb5fd488d0308890a9f101131cde915
powchang1 <115601519+powchang1@users.noreply.github.com>
2022-11-18 07:30:56 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,3 +98,18 @@ submitBtn.addEventListener('click', () => {
         })
  
 });
+
+
+function sendEmail() {
+	Email.send({
+	Host: "smtp.gmail.com",
+	Username : "<sender’s email address>",
+	Password : "<email password>",
+	To : '<recipient’s email address>',
+	From : "<sender’s email address>",
+	Subject : "<email subject>",
+	Body : "<email body>",
+	}).then(
+		message => alert("mail sent successfully")
+	);
+}
